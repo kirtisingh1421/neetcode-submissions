e8c50467e018253e1bfa8a703337cbedfbe805e9
KIRTI SINGH <kirti.singhh2005@gmail.com>
2026-07-07 12:17:54 +0530

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverseBits(int n) {
+        int ans = 0;
+        for(int i = 0;i<32;i++){
+            int lastbit = n &1;
+            ans = ans <<1;
+            ans =  ans |lastbit;
+            n = n >>> 1;
+        }
+        return ans;
+    }
+}
